964c329b86e0e78138aa70547abb4bf185e03da3
Arzaan <arzaansyed2@gmail.com>
2026-06-16 09:36:59 +0530

    Intro to the use of attributes and methods.

---
+++ OOPs/Into.py
@@ -0,0 +1,10 @@
+
+class Car:
+    color = "red"
+    model = "GPU-789"
+    def intro():
+        print('Hello , what would u like to see?')
+
+print(Car.color)
+print(Car.model)
+print(Car.intro())
